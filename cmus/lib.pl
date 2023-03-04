f675a4b492af943daedcc7381f38ea5144ba5e38
Keith James <keithytjames@gmail.com>
2023-03-04 01:37:36 +0000

    Adding all files

---
+++ cmus/lib.pl
@@ -0,0 +1,2 @@
+/home/kj/music/playlists/rhymes/Jack Harlow - WHATS POPPIN.mp3
+/home/kj/music/playlists/rhymes/Kendrick Lamar - N95.mp3
---
+++ doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
